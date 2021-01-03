b875eb48f9560a58ab16de311a287a621cf074af
Mainak Debnath <debnath.mainak07@gmail.com>
2021-01-03 11:46:35 +0530

    updates in settings.py

--- MunchIn/settings.py
+++ MunchIn/settings.py
@@ -56,6 +56,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 ROOT_URLCONF = 'MunchIn.urls'
 
 TEMPLATES = [
